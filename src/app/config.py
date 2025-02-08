298ee49d8aefc8f2e22cfbf1a8ef6a54b4b59df9
Thamer Al-harbi <33045743+thxa@users.noreply.github.com>
2025-02-08 06:40:59 +0300

    Clean up

--- src/app/config.py
+++ src/app/config.py
@@ -9,6 +9,6 @@ class Settings:
     }
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
     RATE_LIMIT_TIMES = int(os.getenv("RATE_LIMIT_TIMES", 10))
-    RATE_LIMIT_SECONDS = int(os.getenv("RATE_LIMIT_SECONDS", 600))  # 10 minutes
+    RATE_LIMIT_SECONDS = int(os.getenv("RATE_LIMIT_SECONDS", 60 * 10))  # 10 minutes
 
 settings = Settings()
